e698d9be0d5af2544bb0c2546d6ee5ea180d2b75
Depressuki <43765913+Depressuki@users.noreply.github.com>
2019-03-14 15:54:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.channel.send('pong')
   }
+  if (msg.content === 'pong') {
+    msg.channel.send("No, I'm supposed to say pong. You say ping, baka.")
+  }
 })
 
 client.login(process.env.BOT_TOKEN)
